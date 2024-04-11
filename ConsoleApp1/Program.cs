6286649966908ec6c4313c8a9805c72b7d4e1229
Kingchenkong <wikeyno@gmail.com>
2024-04-11 17:47:05 +0800

    Add basic hello world project

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World! 2024_0411 17:26");
+
+
+// use this command to run code in terminal:
+// $ dotnet run .
